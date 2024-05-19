d4e57a9874f712b25871adee85b11d725cfe593a
vivrehal <dhimanvivek777@gmail.com>
2024-05-19 13:37:34 +0530

    added dependencies, added basic broiler plate code for express

---
+++ server/index.js
@@ -0,0 +1,22 @@
+import { app } from './app.js';
+import { connectToDb } from './db.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const startServer = () => {
+    connectToDb().then(() => {  
+            app.listen(process.env.PORT || 5000 , () => {
+                console.log('Server running on port ' + process.env.PORT || '5000');
+            });
+    }).catch((err) => {
+        console.log('error connecting to db' + err);
+    });
+}
+
+startServer();
+
+// addUser({name: 'John Doe', email: 'abc@gmail.com', password: '123456', isAdmin: 0, dob: '1990-01-01', token: '123456'});
+
+
+
