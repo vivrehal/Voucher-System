85cc6e0beaa9354028aff72112dd83b05f619323
vivrehal <dhimanvivek777@gmail.com>
2024-05-19 13:38:38 +0530

    setup routers, mysql, env, cors and body parser

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+
+app.use(express.json());
+app.use(cors(
+    {origin: '*'}
+));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Voucher System Backend');
+});
+
+//Routes
+import voucherRouter from './routes/voucherRoutes.js';
+import userRouter from './routes/userRoutes.js';
+
+app.use('/api/v1/vouchers', voucherRouter);
+app.use('/api/v1/users', userRouter);
+
+export {app}
+
