1f65d134e85d0b1b192bea43a022339481408cda
vivrehal <dhimanvivek777@gmail.com>
2024-05-22 12:38:46 +0530

    Added generateToken utility function for JWT token generation

---
+++ server/utils/generateToken.js
@@ -0,0 +1,9 @@
+import jwt from 'jsonwebtoken';
+
+const generateToken = (user) => {
+    return jwt.sign(user, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    });
+}
+
+export {generateToken}
