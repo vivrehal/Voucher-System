5b2cca126cc8c06fe82f7a2f6e42d43dc7c46b5d
vivrehal <dhimanvivek777@gmail.com>
2024-05-22 12:37:26 +0530

    Add auth and isAdmin middleware

---
+++ server/middlewares/auth.js
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import { getUserById } from '../models/userSchema.js';
+import { ApiResponse } from '../utils/apiResponse.js';
+
+const verifyUser = async (req, res, next) => {  
+    const token=req.cookies?.token;
+    if(token){
+        const user= jwt.verify(token, process.env.JWT_SECRET);
+        if(!user){
+            res.status(403).json(new ApiResponse(403, null, 'Unauthorized'));
+            return
+        }
+        const isUser = await getUserById(user.id);   
+        if(!isUser){
+            res.status(403).json(new ApiResponse(403, null, 'Unauthorized'));
+            return
+        }
+        // console.log(user)
+        req.body.userId = isUser.id;
+        next();
+    }else{
+        res.status(403).json(new ApiResponse(403, null, 'Unauthorized'));
+    }
+}
+
+export {verifyUser}
---
+++ server/middlewares/isAdmin.js
@@ -0,0 +1,18 @@
+import { getUserById } from "../models/userSchema.js";
+import { ApiResponse } from "../utils/apiResponse.js";
+
+const isUserAdmin = async(req, res, next) => {
+    if(req.body.userId){
+        const user = await getUserById(req.body.userId);
+        if(user.isAdmin){
+            next();
+        }
+        else{
+            res.status(403).json(new ApiResponse(403, null, 'Unauthorized'));
+        }
+    }else{
+        res.status(403).json(new ApiResponse(403, null, 'Unauthorized'));
+    }
+}
+
+export {isUserAdmin}
